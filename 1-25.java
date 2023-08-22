fc3876425d1e425ed02c4e5b4e8f362396c74f36
Sanjanapatil17 <sanjump1707@gmail.com>
2023-08-23 00:12:12 +0530

    demo

---
+++ 1-25.java
@@ -0,0 +1,24 @@
+class Sanjana
+{
+    public static void main(String[] args)
+    {
+    int i;
+    int j;
+    int n=5;
+    int count=1;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            if (count<10)
+            { 
+            System.out.print("0");
+            }
+        System.out.print(count + " ");
+        //Ashwini
+        count++;
+        }
+    System.out.println();
+    }
+    }
+}
